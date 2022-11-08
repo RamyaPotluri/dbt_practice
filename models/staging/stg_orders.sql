79620dc0ae197127dc01a0ef76c89ca12a15b7d3
ramyapotluri1106@gmail.com <ramyapotluri1106@gmail.com>
2022-11-08 13:07:27 +0000

    developing staging table for orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select o.ORDERID ,
+	o.ORDERDATE ,
+	o.SHIPDATE,
+	o.SHIPMODE,
+     o.ORDERSELLINGPRICE-o.ORDERCOSTPRICE as orderprofits,
+     c.CUSTOMERNAME,
+     c.segment,
+     c.COUNTRY,
+     p.PRODUCTNAME,
+     p.CATEGORY,
+     p.subCATEGORY
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customers') }} as c
+on o.CUSTOMERID=c.CUSTOMERID
+left join {{ ref('raw_product') }} as p 
+on o.PRODUCTID=p.PRODUCTID
